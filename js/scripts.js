dbf8d76dd2503097e7d99f73ae7f1b74a6444b81
David Linke <linke@davelinke.com>
2016-01-08 00:09:46 -0500

    day 2 code
    Changed the feed from a private one to a public one,
    coded all the lightbox navigation routines,
    removed minified files not needed for this exercise

--- js/scripts.js
+++ js/scripts.js
@@ -1,78 +1,165 @@
 var pics = function(){
-    this.clickThumbnail = function(e){
+    var root = this; // we declare as variable so that we can use it without clashing with other "this" scopes
+
+    // the function to go back and forth within the lightbox
+    root.move = function(imageDiv,titleDiv,addendum){
+        var
+            imgData = imageDiv.dataset,
+            currentImage = imgData.imageOrder,
+            nextImage = parseInt(currentImage) + addendum,
+            nextImageElm = document.querySelector('.flickr-photo-set[data-user-id="'+imgData.imageAlbum+'"] [data-image-order="'+nextImage+'"]'),
+            nextImageData = (nextImageElm?nextImageElm.dataset:null),
+            imgParent = imageDiv.parentElement,
+            ipcl = imgParent.classList
+        ;
+        if (nextImageData){
+            // we change stuff
+            imageDiv.setAttribute('data-image-order',nextImageData.imageOrder);
+            imageDiv.style.backgroundImage = 'url('+nextImageData.imageLarge+')';
+            titleDiv.innerHTML = nextImageData.imageTitle;
+
+            //now we modify the lightbox class for the arrows to inherit the proper first/last styles
+            if (parseInt(nextImageData.imageOrder)===0){
+                ipcl.add('at-zero');
+            } else {
+                if (ipcl.contains('at-zero')>-1) ipcl.remove('at-zero');
+            }
+            if (nextImageData.imageLast=="true") {
+                ipcl.add('at-end');
+            } else {
+                if (ipcl.contains('at-end')>-1) ipcl.remove('at-end');
+            }
+        }
+    };
+
+    // a little helper to create buttons
+    root.createButton = function(args){
+        var button = document.createElement('button');
+        button.setAttribute('type','button');
+        button.setAttribute('id',args.id);
+        button.setAttribute('class',args.className);
+        button.innerHTML = args.html;
+        button.addEventListener('click',args.clickFunction);
+        return button;
+    };
+
+    // the function that fires up when clicking thumbs
+    root.clickThumbnail = function(e){
         this.classList.add('active');
-        var lightbox = document.createElement('div');
-        lightbox.classList.add('pics-lightbox');
 
-        var closeButton = document.createElement('button');
-        closeButton.setAttribute('id','pics-lightbox-close');
-        closeButton.setAttribute('type','button');
-        closeButton.innerHTML = '<span class="screen-readers-only">Close Lightbox</span>';
-        closeButton.addEventListener('click',function(){
-            this.parentElement.remove();
-            var activePic = document.querySelector('.flickr-photo-thumb.active');
-            activePic.focus();
-            activePic.classList.remove('active');
+        // we create the lightbox structure here so that the developer does not have to do it (you know, easy stuff)
+        var
+            lightbox = document.createElement('div'),
+            extraClass = (this.dataset.imageOrder=="0"?' at-zero':(this.dataset.imageLast=='true'?' at-end':''))
+        ;
+        lightbox.setAttribute('class','pics-lightbox'+extraClass);
+
+        var closeButton = root.createButton({
+            id:'pics-lightbox-close',
+            html:'<span class="screen-readers-only">Close Lightbox</span>',
+            clickFunction:function(){
+                this.parentElement.remove();
+                var activePic = document.querySelector('.flickr-photo-thumb.active');
+                activePic.focus();
+                activePic.classList.remove('active');
+            },
+            className:'pics-lightbox-close'
         });
-        closeButton.classList.add('pics-lightbox-close');
-
 
         var imgDiv = document.createElement('div');
         imgDiv.classList.add('pics-lightbox-image');
-        imgDiv.style.backgroundImage = 'url('+this.dataset.bigImage+')';
+        imgDiv.setAttribute('id','pics-lightbox-image');
+        imgDiv.setAttribute('title',this.dataset.imageTitle);
+        imgDiv.setAttribute('data-image-order',this.dataset.imageOrder);
+        imgDiv.setAttribute('data-image-album',this.dataset.imageAlbum);
+        imgDiv.style.backgroundImage = 'url('+this.dataset.imageLarge+')';
 
+        var picTitle = document.createElement('span');
+        picTitle.classList.add('pics-lightbox-title');
+        picTitle.innerHTML = this.dataset.imageTitle;
+        picTitle.setAttribute('aria-hidden','true');
+        picTitle.setAttribute('id','pics-lightbox-title');
+
+        var prevButton = root.createButton({
+            id:'pics-lightbox-prev',
+            html:'<span class="screen-readers-only">Previous Picture</span>',
+            clickFunction:function(){root.move(imgDiv,picTitle,-1);},
+            className:'pics-lightbox-arrow pics-lightbox-prev'
+        });
+        var nextButton = root.createButton({
+            id:'pics-lightbox-next',
+            html:'<span class="screen-readers-only">Next Picture</span>',
+            clickFunction:function(){root.move(imgDiv,picTitle,1);},
+            className:'pics-lightbox-arrow pics-lightbox-next'
+        });
 
         lightbox.appendChild(closeButton);
         lightbox.appendChild(imgDiv);
+        lightbox.appendChild(prevButton);
+        lightbox.appendChild(picTitle);
+        lightbox.appendChild(nextButton);
         document.getElementsByTagName('body')[0].appendChild(lightbox);
+
+        // we focus the first button in the lightbox
         document.querySelector('#pics-lightbox-close').focus();
     };
-    this.renderAlbum=jsonFlickrApi=function(data){
+
+    // the album rendering function (also the flickr callback)
+    root.renderAlbum=jsonFlickrFeed=function(data){
         console.log(data);
-        if(data.stat=='ok'){
+        if(data){
             var
-                ps = data.photoset,
-                wrap = document.querySelector('.flickr-photo-set[data-photo-set="'+ps.id+'"]'),
-                photos = ps.photo
+                id = data.link.split('/')[4],
+                wrap = document.querySelector('.flickr-photo-set[data-user-id="'+id+'"]'),
+                photos = data.items,
+                pLength = photos.length
             ;
-            for (var j=0;j<photos.length;j++){
+            // we erase the loading message
+            wrap.innerHTML='';
+            for (var j=0;j<pLength;j++){
                 var
                     photo = photos[j],
-                    photoDiv = document.createElement('button')
+                    photoDiv = document.createElement('button'),
+                    photoTitle = (photo.title===""?'Untitled':photo.title)
                 ;
-                /*
-                pic url format
-                https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}_[mstzb].jpg
-                */
+
+                //pic url format: https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}_[mstzb].jpg
                 photoDiv.classList.add('flickr-photo-thumb');
-                photoDiv.innerHTML = '<span class="photo-title">'+photo.title+'</span>';
-                photoDiv.style.backgroundImage = 'url(https://farm'+photo.farm+'.staticflickr.com/'+photo.server+'/'+photo.id+'_'+photo.secret+'_z.jpg)';
-                photoDiv.setAttribute('data-big-image','https://farm'+photo.farm+'.staticflickr.com/'+photo.server+'/'+photo.id+'_'+photo.secret+'_b.jpg');
-                photoDiv.addEventListener("click", this.clickThumbnail);
+                photoDiv.innerHTML = '<span class="photo-title">'+photoTitle+'</span>';
+                photoDiv.style.backgroundImage = 'url('+photo.media.m.replace('_m.','_z.')+')';
+                photoDiv.setAttribute('data-image-large',photo.media.m.replace('_m.','_b.'));
+                photoDiv.setAttribute('data-image-order',j);
+                photoDiv.setAttribute('data-image-album',id);
+                photoDiv.setAttribute('data-image-title',photoTitle);
+                photoDiv.setAttribute('data-image-last',(j==(pLength-1)?'true':'false'));
+                photoDiv.addEventListener("click", root.clickThumbnail);
                 wrap.appendChild(photoDiv);
             }
-            console.log(wrap);
-        } else {
-            alert(data.message);
+            // we add the class done so that if the routine gets called again, does not pick up this div more than once
+            wrap.classList.add('done');
         }
     };
-    this.getData=function(atts){
+
+    // hello flickr, it's me
+    root.getData=function(album,atts){
         console.log(atts);
-        if(atts.photoSet && atts.apiKey && atts.apiSignature){
+        if(atts.userId){
             var jsonP = document.createElement('script');
             jsonP.src = ''+
-                'https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&'+
-                'api_key='+atts.apiKey+
-                '&photoset_id='+atts.photoSet+
-                '&format=json&api_sig='+atts.apiSignature
+                'https://api.flickr.com/services/feeds/photos_faves.gne?id='+atts.userId +
+                '&format=json'
             ;
-            document.getElementsByTagName('head')[0].appendChild(jsonP);
+            document.getElementsByTagName('body')[0].appendChild(jsonP);
         } else {
-            alert('you are missing data');
+            album.innerHTML = 'Please provide a Flickr user id in the data-user-id attribute of this HTML element';
         }
     };
+
+    // we initialize the routine
+
+    // and the local vars
     var
-        albums = document.querySelectorAll('.flickr-photo-set'),
+        albums = document.querySelectorAll('.flickr-photo-set:not(.done)'), //we run through those that havent been rendered'
         isTouchDevice = (('ontouchstart' in window) || (navigator.MaxTouchPoints > 0) || (navigator.msMaxTouchPoints > 0))
     ;
     for (var i = 0; i<albums.length; i++){
@@ -80,7 +167,12 @@ var pics = function(){
             album = albums[i],
             albumData = album.dataset
         ;
+        // if it's touchable we add the touchable class to the body so that we do not have :hover pseudos for touch devices
         if(isTouchDevice) document.getElementsByTagName('body')[0].classList.add("is-touchable");
-        this.getData(albumData);
+
+        // we display a loading message
+        album.innerHTML = 'Loading data from FLICKR...';
+        // we call flickr on the phone.
+        root.getData(album,albumData);
     }
 }();
